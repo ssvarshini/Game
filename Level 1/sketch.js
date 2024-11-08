8fa770d6cb45dd8e731bfa403fa790c553bc8589
kash2806 <151602527+kash2806@users.noreply.github.com>
2024-11-08 20:40:57 +0800

    akash
    i have changed the location and resize the character

--- Level 1/sketch.js
+++ Level 1/sketch.js
@@ -227,7 +227,7 @@ class Scene {
 
         // Last row
         rect(640, 640, 180, 30, 20);
-        ellipse(200, 660, 100, 100);
+        ellipse(400, 660, 100, 100);
         ellipse(1200, 660, 100, 100);
     }
 }
@@ -241,37 +241,44 @@ class Boy {
     }
 
     draw() {
+        //anchor point
         fill(this.colors.red);
         ellipse(this.anchorX, this.anchorY, 20, 20);
-
+    
+        //body
         fill(this.colors.black);
-        rect(this.anchorX - 28, this.anchorY - 10, 55, 80, 20);
-
+        rect(this.anchorX - 28, this.anchorY + 1, 55, 80, 20);
+        
+        //head
         fill(this.colors.white);
-        ellipse(this.anchorX, this.anchorY - 50, 80, 80);
+        ellipse(this.anchorX, this.anchorY - 40, 80, 80);
 
         fill(this.colors.black);
         ellipse(this.anchorX - 15, this.anchorY - 55, 10, 10);
         ellipse(this.anchorX + 15, this.anchorY - 55, 10, 10);
-
+         
+        //smile
         noFill();
         stroke(this.colors.black);
         strokeWeight(2);
         arc(this.anchorX, this.anchorY - 30, 30, 20, 0, PI);
 
+        //hands
         fill(this.colors.white);
-        ellipse(this.anchorX - 33, this.anchorY + 20, 20, 20);
-        ellipse(this.anchorX + 32, this.anchorY + 20, 20, 20);
-
+        ellipse(this.anchorX - 33, this.anchorY + 30, 20, 20);
+        ellipse(this.anchorX + 32, this.anchorY + 30, 20, 20);
+       
+        //feet
         fill(this.colors.red);
         rect(this.anchorX - 23, this.anchorY + 75, 20, 10);
         rect(this.anchorX + 2, this.anchorY + 75, 20, 10);
-
+       
+        //dumbell
         fill(this.colors.darkRed);
-        rect(this.anchorX - 53, this.anchorY - 10, 100, 20, 5);
+        rect(this.anchorX - 53, this.anchorY +10, 100, 20, 5);
         fill(50);
-        ellipse(this.anchorX - 73, this.anchorY, 40, 80);
-        ellipse(this.anchorX + 67, this.anchorY, 40, 80);
+        ellipse(this.anchorX - 73, this.anchorY+12, 40, 60);
+        ellipse(this.anchorX + 67, this.anchorY+12, 40, 60);
     }
 }
 
@@ -345,115 +352,107 @@ class Orc {
         this.shakeY = this.orcY - 40;
     }
 
-    drawOrc() {
-        // Draw each part of the orc
-        this.drawOrcBody();
-        this.drawOrcHead();
-        this.drawOrcScar();
-        this.drawOrcEyes();
-        this.drawOrcMouth();
-        this.drawOrcLoinclothAndArmor();
-        this.drawOrcArmsAndClub();
-        this.drawOrcLegsAndFeet();
-        this.drawProteinShake();
-    }
+    // drawOrc() {
+    //     // Draw each part of the orc
+    //     this.drawOrcBody();
+    //     this.drawOrcHead();
+    //     this.drawOrcScar();
+    //     this.drawOrcEyes();
+    //     this.drawOrcMouth();
+    //     this.drawOrcLoinclothAndArmor();
+    //     this.drawOrcArmsAndClub();
+    //     this.drawOrcLegsAndFeet();
+    //     this.drawProteinShake();
+    // }
 
-    drawOrcBody() {
-        fill(34, 139, 34); // Green color for the orc skin
-        rect(this.orcX - 30, this.orcY - 30, 60, 80, 10); // Main body rectangle
-    }
+    // drawOrcBody() {
+    //     fill(34, 139, 34); // Green color for the orc skin
+    //     rect(this.orcX - 30, this.orcY - 30, 60, 80, 10); // Main body rectangle
+    // }
 
-    drawOrcHead() {
-        stroke(0);
-        fill(34, 139, 34);
-        ellipse(this.orcX, this.orcY - 60, 60, 50); // Head above the body
-    }
+    // drawOrcHead() {
+    //     stroke(0);
+    //     fill(34, 139, 34);
+    //     ellipse(this.orcX, this.orcY - 60, 60, 50); // Head above the body
+    // }
 
-    drawOrcScar() {
-        strokeWeight(3);
-        stroke(200, 15, 80, 200); // Darker red color for a more pronounced scar
-        line(this.orcX + 5, this.orcY - 75, this.orcX + 25, this.orcY - 55); // Scar line
-    }
+    // drawOrcScar() {
+    //     strokeWeight(3);
+    //     stroke(200, 15, 80, 200); // Darker red color for a more pronounced scar
+    //     line(this.orcX + 5, this.orcY - 75, this.orcX + 25, this.orcY - 55); // Scar line
+    // }
 
-    drawOrcEyes() {
-        fill(0);
-        beginShape();
-        vertex(this.orcX - 15, this.orcY - 70);
-        vertex(this.orcX - 5, this.orcY - 67);
-        vertex(this.orcX - 15, this.orcY - 65);
-        endShape(CLOSE);
+    // drawOrcEyes() {
+    //     fill(0);
+    //     beginShape();
+    //     vertex(this.orcX - 15, this.orcY - 70);
+    //     vertex(this.orcX - 5, this.orcY - 67);
+    //     vertex(this.orcX - 15, this.orcY - 65);
+    //     endShape(CLOSE);
 
-        beginShape();
-        vertex(this.orcX + 5, this.orcY - 70);
-        vertex(this.orcX + 15, this.orcY - 67);
-        vertex(this.orcX + 5, this.orcY - 65);
-        endShape(CLOSE);
+    //     beginShape();
+    //     vertex(this.orcX + 5, this.orcY - 70);
+    //     vertex(this.orcX + 15, this.orcY - 67);
+    //     vertex(this.orcX + 5, this.orcY - 65);
+    //     endShape(CLOSE);
 
-        fill(255, 0, 0); // Red pupils
-        ellipse(this.orcX - 10, this.orcY - 68, 4, 2);
-        ellipse(this.orcX + 10, this.orcY - 68, 4, 2);
-    }
+    //     fill(255, 0, 0); // Red pupils
+    //     ellipse(this.orcX - 10, this.orcY - 68, 4, 2);
+    //     ellipse(this.orcX + 10, this.orcY - 68, 4, 2);
+    // }
 
-    drawOrcMouth() {
-        fill(255); // Fangs
-        triangle(this.orcX - 8, this.orcY - 52, this.orcX - 3, this.orcY - 42, this.orcX - 13, this.orcY - 42);
-        triangle(this.orcX + 8, this.orcY - 52, this.orcX + 13, this.orcY - 42, this.orcX + 3, this.orcY - 42);
+    // drawOrcMouth() {
+    //     fill(255); // Fangs
+    //     triangle(this.orcX - 8, this.orcY - 52, this.orcX - 3, this.orcY - 42, this.orcX - 13, this.orcY - 42);
+    //     triangle(this.orcX + 8, this.orcY - 52, this.orcX + 13, this.orcY - 42, this.orcX + 3, this.orcY - 42);
 
-        stroke(0);
-        strokeWeight(1);
-        fill(0);
-        arc(this.orcX, this.orcY - 47, 30, 15, 0, PI, OPEN); // Mouth
-    }
+    //     stroke(0);
+    //     strokeWeight(1);
+    //     fill(0);
+    //     arc(this.orcX, this.orcY - 47, 30, 15, 0, PI, OPEN); // Mouth
+    // }
 
-    drawOrcLoinclothAndArmor() {
-        fill(169, 169, 169); // Gray shoulder armor
-        arc(this.orcX - 32, this.orcY - 20, 35, 25, PI, 0, CHORD);
-        arc(this.orcX + 32, this.orcY - 20, 35, 25, PI, 0, CHORD);
-    }
+    // drawOrcLoinclothAndArmor() {
+    //     fill(169, 169, 169); // Gray shoulder armor
+    //     arc(this.orcX - 32, this.orcY - 20, 35, 25, PI, 0, CHORD);
+    //     arc(this.orcX + 32, this.orcY - 20, 35, 25, PI, 0, CHORD);
+    // }
 
-    drawOrcArmsAndClub() {
-        fill(34, 139, 34); // Green arms
-        rect(this.orcX - 50, this.orcY - 20, 20, 50); // Left hand
-        rect(this.orcX + 30, this.orcY - 20, 20, 50); // Right hand
+    // drawOrcArmsAndClub() {
+    //     fill(34, 139, 34); // Green arms
+    //     rect(this.orcX - 50, this.orcY - 20, 20, 50); // Left hand
+    //     rect(this.orcX + 30, this.orcY - 20, 20, 50); // Right hand
 
-        fill(139, 69, 19); // Brown color for club
-        rect(this.orcX - 45, this.orcY - 10, 10, 60, 5); // Club handle
-        ellipse(this.orcX - 43, this.orcY + 60, 30, 40); // Club head
-    }
+    //     fill(139, 69, 19); // Brown color for club
+    //     rect(this.orcX - 45, this.orcY - 10, 10, 60, 5); // Club handle
+    //     ellipse(this.orcX - 43, this.orcY + 60, 30, 40); // Club head
+    // }
 
-    drawOrcLegsAndFeet() {
-        fill(34, 139, 34); // Green legs
-        rect(this.orcX - 30, this.orcY + 50, 18, 35, 5);
-        rect(this.orcX + 12, this.orcY + 50, 18, 35, 5);
+    // drawOrcLegsAndFeet() {
+    //     fill(34, 139, 34); // Green legs
+    //     rect(this.orcX - 30, this.orcY + 50, 18, 35, 5);
+    //     rect(this.orcX + 12, this.orcY + 50, 18, 35, 5);
 
-        // Feet
-        arc(this.orcX - 20, this.orcY + 90, 25, 12, PI, 0, CHORD);
-        arc(this.orcX + 20, this.orcY + 90, 25, 12, PI, 0, CHORD);
-    }
+    //     // Feet
+    //     arc(this.orcX - 20, this.orcY + 90, 25, 12, PI, 0, CHORD);
+    //     arc(this.orcX + 20, this.orcY + 90, 25, 12, PI, 0, CHORD);
+    // }
 
-    drawProteinShake() {
-        fill(0, 255, 255); // Blue bottle
-        rect(this.shakeX, this.shakeY, 25, 50, 5);
-        fill(255);
-        rect(this.shakeX + 3, this.shakeY + 5, 19, 15);
-        fill(0, 100, 255);
-        textSize(8);
-        textAlign(CENTER, CENTER);
-        text("protien", this.shakeX + 13, this.shakeY + 30);
+    // drawProteinShake() {
+    //     fill(0, 255, 255); // Blue bottle
+    //     rect(this.shakeX, this.shakeY, 25, 50, 5);
+    //     fill(255);
+    //     rect(this.shakeX + 3, this.shakeY + 5, 19, 15);
+    //     fill(0, 100, 255);
+    //     textSize(8);
+    //     textAlign(CENTER, CENTER);
+    //     text("protien", this.shakeX + 13, this.shakeY + 30);
 
-        fill(105, 45, 20); // Bottle cap
-        rect(this.shakeX, this.shakeY - 5, 25, 5, 2);
-        fill(255);
-        rect(this.shakeX + 15, this.shakeY - 15, 3, 15); // Straw
-    }
-
-    // Method to move the orc based on key presses
-    move(xChange, yChange) {
-        this.orcX += xChange;
-        this.orcY += yChange;
-        this.shakeX += xChange;
-        this.shakeY += yChange;
-    }
+    //     fill(105, 45, 20); // Bottle cap
+    //     rect(this.shakeX, this.shakeY - 5, 25, 5, 2);
+    //     fill(255);
+    //     rect(this.shakeX + 15, this.shakeY - 15, 3, 15); // Straw
+    // }
 }
 
  
