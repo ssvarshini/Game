efab60af0a00d6475ac3b719897cb3f0fc5146a7
kash2806 <151602527+kash2806@users.noreply.github.com>
2024-11-09 13:27:59 +0800

    akash
    minor changes

--- Level 1/sketch.js
+++ Level 1/sketch.js
@@ -1,20 +1,16 @@
-var boy1Left;
-var boy1Right;
-var boy1Up;
-var boy1Down;
-var proteinShakes = []; // Array to hold protein shakes
-var healthbar = 0;
-var lifeStages = []; // Array to hold life stages
-var orcs = []; // Array to hold Orc instances
-var lives = 3; // Number of lives
-var gameOver = false; // Flag to check if the game is over
+// Improved Maze Game Code with Enhancements
 
-function setup() {    
+let moveDirection = { left: false, right: false, up: false, down: false }; // Movement flags
+let proteinShakes = []; // Array to hold protein shakes
+let healthBar = 0;
+let lifeStages = []; // Array to hold life stages
+let orcs = []; // Array to hold Orc instances
+let lives = 3; // Number of lives
+let gameOver = false; // Flag to check if the game is over
+let scene, boy; // Declare scene, boy, properly
+
+function setup() {
     createCanvas(1437, 780);
-    boy1Left = false;
-    boy1Right = false;
-    boy1Up = false;
-    boy1Down = false;
     
     // Colors
     const colors = {
@@ -30,30 +26,35 @@ function setup() {
         darkRed: color(150)
     };
 
-    // Create Scene, Boy, and Knight instances
+    // Create Scene, Boy, and instances
     scene = new Scene(colors);
     boy = new Boy(253, 250, colors);
-   knight = new Knight(150, 150, colors);
     
     // Initialize Orcs with fixed positions
-    orcs.push(new Orc(300,700)); // Orc 1
-    orcs.push(new Orc(400, 250)); // Orc 2
-    orcs.push(new Orc(500, 300)); // Orc 3
-    orcs.push(new Orc(600, 400)); // Orc 4
-    orcs.push(new Orc(1000, 400)); // Orc 5
+    // position of enemies
+    orcs = [
+        new Orc(300, 700), // Orc 1
+        new Orc(400, 250), // Orc 2
+        new Orc(500, 300), // Orc 3
+        new Orc(600, 400), // Orc 4
+        new Orc(1000, 400) // Orc 5
+    ];
 
-    // Initialize protein shakes in the array with different positions
-    proteinShakes.push(new ProteinShake(200, 300)); // Position 1
-    proteinShakes.push(new ProteinShake(600, 200)); // Position 2
-    proteinShakes.push(new ProteinShake(1350, 500)); // Position 3
-    proteinShakes.push(new ProteinShake(250, 600)); // Position 4
-    proteinShakes.push(new ProteinShake(1350, 19));// Position 5
-    proteinShakes.push(new ProteinShake(1300, 300)); // Position 6
-    proteinShakes.push(new ProteinShake(1200, 250)); // Position 7
-    proteinShakes.push(new ProteinShake(900, 350)); // Position 8
+    // Initialize protein shakes with different positions
+    proteinShakes = [
+        new ProteinShake(200, 300),
+        new ProteinShake(600, 200),
+        new ProteinShake(1350, 500),
+        new ProteinShake(250, 600),
+        new ProteinShake(1350, 19),
+        new ProteinShake(1300, 300),
+        new ProteinShake(1200, 250),
+        new ProteinShake(900, 350)
+    ];
+    
     // Initialize life stages
     for (let i = 0; i < 3; i++) {
-        lifeStages.push(new LifeStage(50 + i * 40, 50)); // Adjust position as needed
+        lifeStages.push(new LifeStage(50 + i * 40, 50));
     }
 }
 
@@ -66,33 +67,21 @@ function draw() {
     // Draw Scene, Boy, and Knight
     scene.draw();
     boy.draw();
-    //knight.draw();
-
+    
     // Draw all protein shakes
     for (let shake of proteinShakes) {
         shake.draw();
     }
 
     // Move Boy character based on arrow key flags
-    if (boy1Left) {
-        boy.move(-9, 0); // Move left
-    }
-    if (boy1Right) {
-        boy.move(9, 0); // Move right
-    }
-    if (boy1Up) {
-        boy.move(0, -9); // Move up
-    }
-    if (boy1Down) {
-        boy.move(0, 9); // Move down
-    }   
+    boy.move(moveDirection);
 
     // Check for protein shake collection
     boy.checkCollection(proteinShakes);
     
     // Draw life stages
     for (let i = 0; i < lifeStages.length; i++) {
-        lifeStages[i].draw(lives > i, lives === i); // Fill heart if lives are above the threshold, break if it just lost
+        lifeStages[i].draw(lives > i, lives === i);
     }
     
     // Draw all Orcs and check for collision with the boy
@@ -107,20 +96,17 @@ function draw() {
     drawHealthBar();
 
     // Check if the health bar is full and display level up message
-    if (healthbar === 100) {
-        fill(0, 255, 0); // Green color for success
-        textSize(48);
-        textAlign(CENTER, CENTER);
-        text("Yay, proceed to level 2!", width / 2, height / 2);
+    if (healthBar === 100) {
+        displayLevelUpMessage();
     }
 }
 
 // Method to draw the health bar
 function drawHealthBar() {
-    fill(255, 0, 0); // Red color for the health bar
+    fill(255, 0, 0); // Red color for the health bar background
     rect(20, 20, 200, 20); // Background of the health bar
     fill(0, 255, 0); // Green color for the health
-    rect(20, 20, healthbar * 2, 20); // Health bar (width increases based on health)
+    rect(20, 20, healthBar * 2, 20); // Health bar (width increases based on health)
 }
 
 // Method to display game over
@@ -130,6 +116,14 @@ function displayGameOver() {
     textSize(64);
     textAlign(CENTER, CENTER);
     text("Game Over", width / 2, height / 2);
+}
+
+// Method to display level up message
+function displayLevelUpMessage() {
+    fill(0, 255, 0); // Green color for success
+    textSize(48);
+    textAlign(CENTER, CENTER);
+    text("Yay, proceed to level 2!", width / 2, height / 2);
 }
 
 // Method to handle losing a life
@@ -144,37 +138,41 @@ function loseLife() {
 // LifeStage Class
 class LifeStage {
     constructor(x, y) {
-        this.x = x; // X position for the heart
-        this.y = y; // Y position for the heart
-        this.size = 30; // Size of the heart
+        this.x = x;
+        this.y = y;
+        this.size = 30;
     }
 
     draw(isFilled, isBreaking) {
         if (isBreaking) {
-            fill(200, 0, 0); // Darker red for broken heart
-            beginShape();
-            vertex(this.x, this.y);
-            bezierVertex(this.x - this.size / 2, this.y - this.size / 2, this.x - this.size, this.y + this.size / 2, this.x, this.y + this.size);
-            bezierVertex(this.x + this.size, this.y + this.size / 2, this.x + this.size / 2, this.y - this.size / 2, this.x, this.y);
-            endShape(CLOSE);
-            // Draw a break line
-            stroke(0);
-            strokeWeight(2);
-            line(this.x - this.size / 4, this.y - this.size / 4, this.x + this.size / 4, this.y + this.size / 4);
-            line(this.x + this.size / 4, this.y - this.size / 4, this.x - this.size / 4, this.y + this.size / 4);
-            noStroke();
+            this.drawBreakingHeart();
         } else {
             fill(isFilled ? color(255, 0, 0) : color(200, 0, 0)); // Red for filled, dark red for empty
-            beginShape();
-            vertex(this.x, this.y);
-            bezierVertex(this.x - this.size / 2, this.y - this.size / 2, this.x - this.size, this.y + this.size / 2, this.x, this.y + this.size);
-            bezierVertex(this.x + this.size, this.y + this.size / 2, this.x + this.size / 2, this.y - this.size / 2, this.x, this.y);
-            endShape(CLOSE);
+            this.drawHeartShape();
         }
+    }
+
+    drawBreakingHeart() {
+        fill(200, 0, 0); // Darker red for broken heart
+        this.drawHeartShape();
+        // Draw break lines
+        stroke(0);
+        strokeWeight(2);
+        line(this.x - this.size / 4, this.y - this.size / 4, this.x + this.size / 4, this.y + this.size / 4);
+        line(this.x + this.size / 4, this.y - this.size / 4, this.x - this.size / 4, this.y + this.size / 4);
+        noStroke();
+    }
+
+    drawHeartShape() {
+        beginShape();
+        vertex(this.x, this.y);
+        bezierVertex(this.x - this.size / 2, this.y - this.size / 2, this.x - this.size, this.y + this.size / 2, this.x, this.y + this.size);
+        bezierVertex(this.x + this.size, this.y + this.size / 2, this.x + this.size / 2, this.y - this.size / 2, this.x, this.y);
+        endShape(CLOSE);
     }
 }
 
-// Scene Constructor Function
+// Scene Class
 class Scene {
     constructor(colors) {
         this.colors = colors;
@@ -193,33 +191,36 @@ class Scene {
         noStroke();
         fill(this.colors.tree);
 
+        // Maze Elements
+        this.drawMaze();
+    }
+
+    drawMaze() {
         // First row
-        rect(width / 2, 5, 20, 180); // 1st row middle
-        rect(100, 80, 160, 120, 20); // 1st row 1st
-        rect(370, 80, 200, 120, 20); // 1st row 2nd
-        rect(830, 80, 200, 120, 20); // 1st row 3rd
-        rect(1140, 80, 160, 120, 20); // 1st row 4th
+        rect(width / 2, 5, 20, 180);
+        rect(100, 80, 160, 120, 20);
+        rect(370, 80, 200, 120, 20);
+        rect(830, 80, 200, 120, 20);
+        rect(1140, 80, 160, 120, 20);
 
         // Second row
-        rect(300, height / 2 - 100, 100, 30, 10); // 2nd row 1st
-        rect(590, height / 2 - 100, 270, 30, 10); // 2nd row middle
-        rect(715, height / 2 - 100, 30, 100, 10); // 2nd row middle
-        rect(1050, height / 2 - 100, 100, 30, 10); // 2nd row last
+        rect(300, height / 2 - 100, 100, 30, 10);
+        rect(590, height / 2 - 100, 270, 30, 10);
+        rect(715, height / 2 - 100, 30, 100, 10);
+        rect(1050, height / 2 - 100, 100, 30, 10);
 
         // Full rectangles in row 3
-        rect(140, 390, 200, 30, 20); // 1st full rect
+        rect(140, 390, 200, 30, 20);
         rect(320, 390, 30, 140, 20);
         rect(140, 500, 200, 30, 20);
-
-        rect(1120, 390, 200, 30, 20); // 2nd full rect
+        rect(1120, 390, 200, 30, 20);
         rect(1120, 390, 30, 140, 20);
         rect(1120, 500, 200, 30, 20);
 
         // Center structures
-        rect(470, 390, 30, 205, 20); // Centre 1
+        rect(470, 390, 30, 205, 20);
         rect(490, 450, 100, 30, 20);
-
-        rect(940, 390, 30, 205, 20); // Centre 2
+        rect(940, 390, 30, 205, 20);
         rect(860, 565, 100, 30, 20);
 
         // Last row
@@ -229,19 +230,24 @@ class Scene {
     }
 }
 
+// Boy Class
 class Boy {
     constructor(x, y, colors) {
-        this.anchorX = x / 0.5; 
-        this.anchorY = y / 0.5;
-        this.startX = this.anchorX; // Store initial position
+        this.anchorX = x;
+        this.anchorY = y;
+        this.startX = this.anchorX;
         this.startY = this.anchorY;
         this.colors = colors;
     }
 
     draw() {
-        push(); 
-        scale(0.5); 
+        push();
+        scale(0.5);
+        this.drawCharacter();
+        pop();
+    }
 
+    drawCharacter() {
         // Anchor Point
         fill(this.colors.red);
         ellipse(this.anchorX, this.anchorY, 20, 20);
@@ -255,41 +261,57 @@ class Boy {
         ellipse(this.anchorX, this.anchorY - 40, 80, 80);
 
         // Eyes
-        fill(this.colors.black);
-        ellipse(this.anchorX - 15, this.anchorY - 55, 10, 10);
-        ellipse(this.anchorX + 15, this.anchorY - 55, 10, 10);
+        this.drawEyes();
 
         // Smile
-        noFill();
-        stroke(this.colors.black);
-        strokeWeight(2);
-        arc(this.anchorX, this.anchorY - 30, 30, 20, 0, PI);
+        this.drawSmile();
 
         // Hands
-        fill(this.colors .white);
+        fill(this.colors.white);
         ellipse(this.anchorX - 33, this.anchorY + 30, 20, 20);
         ellipse(this.anchorX + 32, this.anchorY + 30, 20, 20);
 
         // Feet
+        this.drawFeet();
+
+        // Dumbbell
+        this.drawDumbbell();
+    }
+
+    drawEyes() {
+        fill(this.colors.black);
+        ellipse(this.anchorX - 15, this.anchorY - 55, 10, 10);
+        ellipse(this.anchorX + 15, this.anchorY - 55, 10, 10);
+    }
+
+    drawSmile() {
+        noFill();
+        stroke(this.colors.black);
+        strokeWeight(2);
+        arc(this.anchorX, this.anchorY - 30, 30, 20, 0, PI);
+    }
+
+    drawFeet() {
         fill(this.colors.red);
         rect(this.anchorX - 23, this.anchorY + 75, 20, 10);
         rect(this.anchorX + 2, this.anchorY + 75, 20, 10);
+    }
 
-        // Dumbbell
+    drawDumbbell() {
         fill(this.colors.darkRed);
         rect(this.anchorX - 53, this.anchorY + 10, 100, 20, 5);
         fill(50);
         ellipse(this.anchorX - 73, this.anchorY + 12, 40, 60);
         ellipse(this.anchorX + 67, this.anchorY + 12, 40, 60);
-
-        pop(); // Restore the transformation matrix
     }
 
-    move(xChange, yChange) {
-        this.anchorX += xChange * 2; 
-        this.anchorY += yChange * 2;
+    move(direction) {
+        if (direction.left) this.anchorX -= 18;
+        if (direction.right) this.anchorX += 18;
+        if (direction.up) this.anchorY -= 18;
+        if (direction.down) this.anchorY += 18;
     }
-    
+
     checkCollection(proteinShakes) {
         let scaledX = this.anchorX * 0.5;
         let scaledY = this.anchorY * 0.5;
@@ -299,9 +321,9 @@ class Boy {
             let distance = dist(scaledX, scaledY, shake.shakeX, shake.shakeY);
             
             if (distance < 50) {
-                proteinShakes.splice(i, 1); 
-                healthbar += 20; 
-                if (healthbar > 100) healthbar = 100; 
+                proteinShakes.splice(i, 1);
+                healthBar += 20;
+                if (healthBar > 100) healthBar = 100;
             }
         }
     }
@@ -319,92 +341,34 @@ class Boy {
     }
 }
 
-// Knight Constructor Function
-class Knight {
-    constructor(x, y, colors) {
-        this.anchorX = x;
-        this.anchorY = y;
-        this.colors = colors;
-    }
-
-    draw() {
-        fill(this.colors.red);
-        ellipse(this.anchorX, this.anchorY, 20, 20);
-
-        fill(this.colors.silver);
-        rect(this.anchorX - 35, this.anchorY - 115, 70, 70);
-        fill(this.colors.darkSilver);
-        rect(this.anchorX - 35, this.anchorY - 95, 70, 10);
-        fill(this.colors.black);
-        rect(this.anchorX - 20, this.anchorY - 100, 40, 8);
-        rect(this.anchorX - 35, this.anchorY - 65, 70, 3);
-        rect(this.anchorX - 25, this.anchorY - 55, 50, 3);
-
-        fill(this.colors.red);
-        beginShape();
-        vertex(this.anchorX - 5, this.anchorY - 135);
-        vertex(this.anchorX + 5, this.anchorY - 140);
-        vertex(this.anchorX + 10, this.anchorY - 130);
-        vertex(this.anchorX + 5, this.anchorY - 120);
-        vertex(this.anchorX - 5, this.anchorY - 125);
-        endShape(CLOSE);
-
-        fill(this.colors.silver);
-        rect(this.anchorX - 35, this.anchorY - 40, 70, 90, 5);
-        fill(this.colors.darkSilver);
-        rect(this.anchorX - 35, this.anchorY - 30, 70, 5);
-        fill(this.colors.gold);
-        rect(this.anchorX - 35, this.anchorY - 40, 5, 90);
-        rect(this.anchorX + 30, this.anchorY - 40, 5, 90);
-
-        fill(this.colors.silver);
-        ellipse(this.anchorX - 50, this.anchorY - 45, 30, 30);
-        ellipse(this.anchorX + 50, this.anchorY - 45, 30, 30);
-        rect(this.anchorX - 60, this.anchorY - 30, 20, 50);
-        rect(this.anchorX + 40, this.anchorY - 30, 20, 50);
-        fill(this.colors.black);
-        ellipse(this.anchorX - 50, this.anchorY + 30, 20, 20);
-        ellipse(this.anchorX + 50, this.anchorY + 30, 20, 20);
-
-        fill(this.colors.brown);
-        rect(this.anchorX - 35, this.anchorY + 50, 70, 12);
-        fill(this.colors.gold);
-        rect(this.anchorX - 8, this.anchorY + 50, 15, 12);
-
-        fill(this.colors.silver);
-        rect(this.anchorX - 30, this.anchorY + 70, 27, 10);
-        rect(this.anchorX + 5, this.anchorY + 70, 27, 10);
-    }
-}
-
 function keyPressed() {
     // Arrow keys for the Boy
     if (keyCode === LEFT_ARROW) {
-        boy1Left = true;
+        moveDirection.left = true;
     }
     if (keyCode === RIGHT_ARROW) {
-        boy1Right = true;
+        moveDirection.right = true;
     }
     if (keyCode === UP_ARROW) {
-        boy1Up = true;
+        moveDirection.up = true;
     }
     if (keyCode === DOWN_ARROW) {
-        boy1Down = true;
+        moveDirection.down = true;
     }
 }
 
 function keyReleased() {
     if (keyCode === LEFT_ARROW) {
-        boy1Left = false;
+        moveDirection.left = false;
     }
     if (keyCode === RIGHT_ARROW) {
-        boy1Right = false;
+        moveDirection.right = false;
     }
     if (keyCode === UP_ARROW) {
-        boy1Up = false;
+        moveDirection.up = false;
     }
     if (keyCode === DOWN_ARROW) {
-        boy1Down = false;
+        moveDirection.down = false;
     }
 }
 
@@ -490,7 +454,7 @@ class Orc {
 
     drawOrcMouth() {
         fill(255); // Fangs
-        triangle(this.orcX - 4, this.orcY - 26, this.orcX -  2, this.orcY - 21, this.orcX - 6, this.orcY - 21);
+        triangle(this.orcX - 4, this.orcY - 26, this.orcX - 2, this.orcY - 21, this.orcX - 6, this.orcY - 21);
         triangle(this.orcX + 4, this.orcY - 26, this.orcX + 6, this.orcY - 21, this.orcX + 2, this.orcY - 21);
 
         stroke(0);
